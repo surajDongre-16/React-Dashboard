8b7e4584156ec603cfd55acc8152a5bbddef5d11
surajDongre-16 <surajdongre8@gmail.com>
2023-05-08 12:13:01 +0530

    basic folder structure and theme context at base level completed

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,13 @@
+import React, {useContext} from 'react'
+import { Box,IconButton,useTheme } from '@mui/material'
+import { ColorModeContext,tokens } from '../../theme'
+import InputBase from '@mui/material'
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+
+const Topbar = () => {
+  return (
+    <div>Topbar</div>
+  )
+}
+
+export default Topbar
