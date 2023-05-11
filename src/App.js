f0faf59df817aa0b9f12a8908639322f5c025b3f
surajDongre-16 <surajdongre8@gmail.com>
2023-05-11 21:29:40 +0530

    geography page ui completed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 import { Route, Routes } from "react-router-dom";
 
@@ -36,7 +36,7 @@ function App() {
               <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/geography" element={<Geography />} />
               <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
