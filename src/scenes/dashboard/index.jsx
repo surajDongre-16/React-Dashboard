1e126ffed9ba81320b998dbd2ef5c675d1cb9694
surajDongre-16 <surajdongre8@gmail.com>
2023-05-12 10:25:10 +0530

    revenue graph ui and transaction table is added onto the dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -20,7 +20,11 @@ const Dashboard = () => {
 
   return (
     <Box m="20px">
-      <Box display="flex" justifyContent={'space-between'} alignItems={'center'}>
+      <Box
+        display="flex"
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
         <Box>
           <Button
@@ -125,6 +129,100 @@ const Dashboard = () => {
         </Box>
 
         {/* row 2 */}
+
+        <Box
+          gridColumn={"span 8"}
+          gridRow={"span 2"}
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                color={colors.gray[100]}
+              >
+                Revenue Generated
+              </Typography>
+              <Typography
+                variant="h3"
+                fontWeight="bold"
+                color={colors.greenAccent[500]}
+              >
+                $59,342,32
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+
+          <Box height="250px" mt="-20px">
+            <LineChart isDashboard={true} />
+          </Box>
+        </Box>
+        {/* transactions */}
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          overflow={"auto"}
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.primary[500]}`}
+            color={colors.gray[100]}
+            p="15px"
+          >
+            <Typography color={colors.gray[100]} variant="h5" fontWeight="600">
+              Recent Transactions
+            </Typography>
+          </Box>
+          {mockTransactions.map((transaction, i) => (
+            <Box
+              key={`${transaction.txId}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems={"center"}
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box>
+                <Typography
+                  color={colors.greenAccent[500]}
+                  variant="h5"
+                  fontWeight="600"
+                >
+                  {transaction.txId}
+                </Typography>
+                <Typography color={colors.gray[100]}>
+                  {transaction.user}
+                </Typography>
+              </Box>
+              <Box color={colors.gray[100]}>{transaction.date}</Box>
+              <Box
+                backgroundColor={colors.greenAccent[500]}
+                p="5px 10px"
+                borderRadius="4px"
+              >
+                ${transaction.cost}
+              </Box>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
